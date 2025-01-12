d87ebc481b10ee5ba607caf9f996e71d3adc814d
Trisha S Shetty <136143475+trishashetty19@users.noreply.github.com>
2025-01-12 20:48:05 +0530

    Time: 2873 ms (5.23%), Space: 17.8 MB (36.18%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False 
+        for letter in s:
+            if s.count(letter) != t.count(letter):
+                return False 
+        return True 
+        
