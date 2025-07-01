dd79829b356735d50ad171d0dffe600552ce7ec5
Trisha S Shetty <136143475+trishashetty19@users.noreply.github.com>
2025-07-01 19:14:39 +0530

    Time: 0 ms (100%), Space: 17.9 MB (8.49%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -8,7 +8,7 @@ class Solution:
         for _ in range(3, n+1):
             t3 = t0+t1+t2
             t0, t1, t2 = t1, t2, t3 #Shiftig the frame
-        return t2
+        return t2                   #t2 holds the value 
         
 
         
