ea58aedbd6e66ec4538f9bcb3a9f2221d9e55ec0
Trisha S Shetty <136143475+trishashetty19@users.noreply.github.com>
2025-07-06 20:47:59 +0530

    Time: 989 ms (5.77%), Space: 24.5 MB (65.25%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,4 +1,47 @@
 class Solution:
+    def merge(slef, nums, l, mid, r):
+        a = []
+        b = []
+
+        for i in range(l,mid+1):
+            a.append(nums[i])
+        for j in range(mid+1, r+1):
+            b.append(nums[j])
+
+        i,j,k = 0,0,l
+        
+        while k<=r:
+            if j==len(b):
+                nums[k] = a[i]
+                i+=1
+                k+=1
+            elif i==len(a):
+                nums[k]= b[j]
+                j+=1
+                k+=1
+            elif a[i]<b[j]:
+                nums[k] = a[i]
+                i+=1
+                k+=1
+            else:
+                nums[k]= b[j]
+                j+=1
+                k+=1
+            
+
+    def mergeSort(self, nums, l , r):
+        #Base case
+        if l>=r:
+            return
+
+        #Recursive case 
+        mid=(l+r)//2
+        self.mergeSort(nums,l,mid)
+        self.mergeSort(nums,mid+1,r)
+
+        self.merge(nums, l, mid, r)
+        
     def sortArray(self, nums: List[int]) -> List[int]:
-        nums.sort()
+        self.mergeSort(nums,0,len(nums)-1)
         return nums
+        
