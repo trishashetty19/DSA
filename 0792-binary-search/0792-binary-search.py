616d9a87d333d122e7ebfd55a2a9d7aff687eb35
Trisha S Shetty <136143475+trishashetty19@users.noreply.github.com>
2025-07-09 19:55:20 +0530

    Time: 0 ms (100%), Space: 18.8 MB (27.25%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.py
@@ -0,0 +1,7 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        for i in range(n):
+            if nums[i] == target:
+                return i
+        return -1
